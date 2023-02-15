9d61e1c625e57d3ef902ab318dfca621b9b0ac2f
tqs470187906 <470187906@qq.com>
2023-02-15 21:22:43 +0800

    新建tcp编程debug文件

--- tcp_udp/tcp.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2023/2/15 20:46
-# @Author  : TQS
-# @File    : tcp.py
---
+++ tcp_udp/tcp_client.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/2/15 20:46
+# @Author  : TQS
+# @File    : tcp_client.py
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('www.sina.com.cn', 80))
+
+s.send(b'GET / HTTP/1.1'
+       b'HOST:www.sina.com.cn'
+       b'Connection:close'
+       b''
+       b'')
+
+buffer = []
+while True:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = b''.join(buffer)
+
+s.close()
+print(data)
+
+header, body = data.split(b'\r\n\r\n', 1)
+print(header.decode('utf-8'))
+with open('sina.html', 'wb') as f:
+    f.write(body)
