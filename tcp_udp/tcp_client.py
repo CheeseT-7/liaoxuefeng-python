e9ea3f99b6f9ebedcea6c020fc89f4b8552c1eb9
cheese <470187906@qq.com>
2023-02-17 21:09:20 +0800

    简单的tcp服务

--- tcp_udp/tcp_client.py
+++ tcp_udp/tcp_client.py
@@ -4,29 +4,41 @@
 # @Author  : TQS
 # @File    : tcp_client.py
 
+# import socket
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(('www.sina.com.cn', 80))
+#
+# s.send(b'GET / HTTP/1.1'
+#        b'HOST:www.sina.com.cn'
+#        b'Connection:close'
+#        b''
+#        b'')
+#
+# buffer = []
+# while True:
+#     d = s.recv(1024)
+#     if d:
+#         buffer.append(d)
+#     else:
+#         break
+# data = b''.join(buffer)
+#
+# s.close()
+#
+# header, body = data.split(b'\r\n\r\n', 1)
+# print(header.decode('utf-8'))
+# with open('sina.html', 'wb') as f:
+#     f.write(body)
+
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.sina.com.cn', 80))
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024).decode('utf-8'))
+for data in [b'A', b'B', b'c']:
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
 
-s.send(b'GET / HTTP/1.1'
-       b'HOST:www.sina.com.cn'
-       b'Connection:close'
-       b''
-       b'')
-
-buffer = []
-while True:
-    d = s.recv(1024)
-    if d:
-        buffer.append(d)
-    else:
-        break
-data = b''.join(buffer)
-
+s.send(b'exit')
 s.close()
-
-header, body = data.split(b'\r\n\r\n', 1)
-print(header.decode('utf-8'))
-with open('sina.html', 'wb') as f:
-    f.write(body)
